3f02ba7323414362d8ecd90b3fa611c289525336
Adam Faulconbridge <afaulconbridge@googlemail.com>
2016-02-24 16:25:07 +0000

    Fix to generic class reference

--- webapp/src/main/java/uk/ac/ebi/biosamples/relations/webapp/repo/OwnedByRepository.java
+++ webapp/src/main/java/uk/ac/ebi/biosamples/relations/webapp/repo/OwnedByRepository.java
@@ -3,7 +3,9 @@ package uk.ac.ebi.biosamples.relations.webapp.repo;
 import org.springframework.data.neo4j.repository.GraphRepository;
 import org.springframework.stereotype.Repository;
 
+import uk.ac.ebi.biosamples.relations.model.edges.OwnedBy;
+
 @Repository
-public interface OwnedByRepository extends GraphRepository<OwnedByRepository> {
+public interface OwnedByRepository extends GraphRepository<OwnedBy> {
 	
 }
