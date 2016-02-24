2ab2b645e0ca7fa8a77fff9468a9b756b9c673e2
Adam Faulconbridge <afaulconbridge@googlemail.com>
2016-02-24 11:20:09 +0000

    Cleanup unused imports

--- importrelational/src/main/java/uk/ac/ebi/biosamples/relations/importbiosdmodel/Application.java
+++ importrelational/src/main/java/uk/ac/ebi/biosamples/relations/importbiosdmodel/Application.java
@@ -2,7 +2,6 @@ package uk.ac.ebi.biosamples.relations.importbiosdmodel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.orm.jpa.EntityScan;
 
 @SpringBootApplication
 public class Application {
