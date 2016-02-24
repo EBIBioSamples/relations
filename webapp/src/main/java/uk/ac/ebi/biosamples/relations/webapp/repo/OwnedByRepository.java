5c0e47a31faf048725893c04105b951f37e43cbf
Adam Faulconbridge <afaulconbridge@googlemail.com>
2016-02-24 16:26:09 +0000

    Tweaked pluralization of ownedby

--- webapp/src/main/java/uk/ac/ebi/biosamples/relations/webapp/repo/OwnedByRepository.java
+++ webapp/src/main/java/uk/ac/ebi/biosamples/relations/webapp/repo/OwnedByRepository.java
@@ -1,11 +1,13 @@
 package uk.ac.ebi.biosamples.relations.webapp.repo;
 
 import org.springframework.data.neo4j.repository.GraphRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import uk.ac.ebi.biosamples.relations.model.edges.OwnedBy;
 
 @Repository
+@RepositoryRestResource(collectionResourceRel = "ownedBys", path = "ownedBys")
 public interface OwnedByRepository extends GraphRepository<OwnedBy> {
 	
 }
