fe08d9e0ebc3d0636021d210e04a7a32427d24d3
Adam Faulconbridge <afaulconbridge@googlemail.com>
2016-03-08 16:41:37 +0000

    First commit of jdbc

---
+++ importjdbc/src/main/java/uk/ac/ebi/biosamples/relations/importjdbc/Application.java
@@ -0,0 +1,29 @@
+package uk.ac.ebi.biosamples.relations.importjdbc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@SpringBootApplication
+public class Application implements ApplicationRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String args[]) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public void run(ApplicationArguments args) throws Exception {
+
+        log.info("Reading tables");
+		
+	}
+}
