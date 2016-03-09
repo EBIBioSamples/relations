13706c7961994f3f741ff5fa178d9963c721b1ce
Adam Faulconbridge <afaulconbridge@googlemail.com>
2016-03-09 17:13:14 +0000

    Added generation of derivation edges
    Note, this is a *very* slow process

--- importjdbc/src/main/java/uk/ac/ebi/biosamples/relations/importjdbc/Application.java
+++ importjdbc/src/main/java/uk/ac/ebi/biosamples/relations/importjdbc/Application.java
@@ -8,6 +8,13 @@ import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
@@ -20,6 +27,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowCallbackHandler;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
 public class Application implements ApplicationRunner {
@@ -29,8 +37,11 @@ public class Application implements ApplicationRunner {
 	}
 
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
+	
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
+	
+	private List<Future<Void>> futures = new ArrayList<>();
 
 	private static class CSVCallbackHandler implements RowCallbackHandler {
 		private final Logger log = LoggerFactory.getLogger(this.getClass());
@@ -54,50 +65,79 @@ public class Application implements ApplicationRunner {
 			}
 		}
 	}
+	
+	
+	private final class FileCallable implements Callable<Void> {
 
+		private final String filename;
+		private final String[] headers;
+		private final String sql;
+		
+		public FileCallable(String filename, String[] headers, String sql) {
+			this.filename = filename;
+			this.headers = headers;
+			this.sql = sql;
+		}
+		
+		@Override
+		public Void call() throws Exception {
+			try (final CSVPrinter csvPrinter = new CSVPrinter(new FileWriter(new File(filename)),
+					CSVFormat.DEFAULT.withHeader(headers))) {
+				jdbcTemplate.query(sql, new CSVCallbackHandler(csvPrinter));
+			}
+			return null;
+		}
+		
+	}
+
+	@Transactional
 	public void run(ApplicationArguments args) throws Exception {
 
-		try (final CSVPrinter csvPrinter = new CSVPrinter(new FileWriter(new File("samples.csv")),
-				CSVFormat.DEFAULT.withHeader("accession:ID(Sample)"))) {
-			log.info("Running samples query");
-			String sql = "SELECT ACC FROM BIO_PRODUCT";
-			jdbcTemplate.query(sql, new CSVCallbackHandler(csvPrinter));
-		}
+		ExecutorService threadPool = null; 
+		try {
+			threadPool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+			String sql;
+			
+			sql = "SELECT ACC FROM BIO_PRODUCT";
+			futures.add(threadPool.submit(new FileCallable("samples.csv", new String[]{"accession:ID(Sample)"}, sql)));
+			
+			sql = "SELECT ACC FROM BIO_SMP_GRP";
+			futures.add(threadPool.submit(new FileCallable("groups.csv", new String[]{"accession:ID(Group)"}, sql)));
+			
+			sql = "SELECT ACC FROM MSI";
+			futures.add(threadPool.submit(new FileCallable("submissions.csv", new String[]{"submissionId:ID(Submission)"}, sql)));
+			
+			sql = "SELECT BIO_SMP_GRP.ACC, MSI.ACC FROM BIO_SMP_GRP JOIN MSI_SAMPLE_GROUP ON BIO_SMP_GRP.ID = MSI_SAMPLE_GROUP.GROUP_ID JOIN MSI ON MSI.ID = MSI_SAMPLE_GROUP.MSI_ID";
+			futures.add(threadPool.submit(new FileCallable("group_ownership.csv", new String[]{":START_ID(Group)", ":END_ID(Submission)"}, sql)));
+			
+			sql = "SELECT BIO_PRODUCT.ACC, MSI.ACC FROM BIO_PRODUCT JOIN MSI_SAMPLE ON BIO_PRODUCT.ID = MSI_SAMPLE.SAMPLE_ID JOIN MSI ON MSI.ID = MSI_SAMPLE.MSI_ID";
+			futures.add(threadPool.submit(new FileCallable("sample_ownership.csv", new String[]{":START_ID(Sample)", ":END_ID(Submission)"}, sql)));
+			
+			sql = "SELECT BIO_PRODUCT.ACC, BIO_SMP_GRP.ACC FROM BIO_SMP_GRP JOIN BIO_SAMPLE_SAMPLE_GROUP ON BIO_SMP_GRP.ID = BIO_SAMPLE_SAMPLE_GROUP.GROUP_ID JOIN BIO_PRODUCT ON BIO_PRODUCT.ID = BIO_SAMPLE_SAMPLE_GROUP.SAMPLE_ID";
+			futures.add(threadPool.submit(new FileCallable("sample_membership.csv", new String[]{":START_ID(Sample)", ":END_ID(Group)"}, sql)));
+			
+			sql = "SELECT b.ACC, b2.ACC FROM BIO_PRODUCT b JOIN PRODUCT_PV pv ON b.ID = pv.OWNER_ID JOIN EXP_PROP_VAL epv ON pv.PV_ID = epv.ID JOIN EXP_PROP_TYPE ept ON epv.TYPE_ID = ept.ID JOIN BIO_PRODUCT b2 ON epv.TERM_TEXT LIKE b2.ACC WHERE REGEXP_LIKE(ept.TERM_TEXT, 'Derived From', 'i')";
+			futures.add(threadPool.submit(new FileCallable("sample_derivation.csv", new String[]{":START_ID(Sample)", ":END_ID(Sample)"}, sql)));
 
-		try (final CSVPrinter csvPrinter = new CSVPrinter(new FileWriter(new File("groups.csv")),
-				CSVFormat.DEFAULT.withHeader("accession:ID(Group)"))) {
-			log.info("Running groups query");
-			String sql = "SELECT ACC FROM BIO_SMP_GRP";
-			jdbcTemplate.query(sql, new CSVCallbackHandler(csvPrinter));
+	        //wait for all other futures to finish
+	        log.info("Waiting for futures...");
+			for (Future<Void> future : futures) {
+				try {
+					future.get();
+				} catch (ExecutionException e) {
+					log.error("Problem getting future", e);
+				}
+			}
+			
+		} finally {
+			//handle closing of thread pool in case of error
+			if (threadPool != null && !threadPool.isShutdown()) {
+		        log.info("Shutting down thread pool");
+		        //allow a second to cleanly terminate before forcing
+		        threadPool.shutdown();
+				threadPool.awaitTermination(1, TimeUnit.SECONDS);
+				threadPool.shutdownNow();
+			}
 		}
-
-		try (final CSVPrinter csvPrinter = new CSVPrinter(new FileWriter(new File("submissions.csv")),
-				CSVFormat.DEFAULT.withHeader("submissionId:ID(Submission)"))) {
-			log.info("Running submissions query");
-			String sql = "SELECT ACC FROM MSI";
-			jdbcTemplate.query(sql, new CSVCallbackHandler(csvPrinter));
-		}
-
-		try (final CSVPrinter csvPrinter = new CSVPrinter(new FileWriter(new File("group_ownership.csv")),
-				CSVFormat.DEFAULT.withHeader(":START_ID(Group)", ":END_ID(Submission)"))) {
-			log.info("Running group ownership query");
-			String sql = "SELECT BIO_SMP_GRP.ACC, MSI.ACC FROM BIO_SMP_GRP JOIN MSI_SAMPLE_GROUP ON BIO_SMP_GRP.ID = MSI_SAMPLE_GROUP.GROUP_ID JOIN MSI ON MSI.ID = MSI_SAMPLE_GROUP.MSI_ID";
-			jdbcTemplate.query(sql, new CSVCallbackHandler(csvPrinter));
-		}
-
-		try (final CSVPrinter csvPrinter = new CSVPrinter(new FileWriter(new File("sample_ownership.csv")),
-				CSVFormat.DEFAULT.withHeader(":START_ID(Sample)", ":END_ID(Submission)"))) {
-			log.info("Running sample ownership query");
-			String sql = "SELECT BIO_PRODUCT.ACC, MSI.ACC FROM BIO_PRODUCT JOIN MSI_SAMPLE ON BIO_PRODUCT.ID = MSI_SAMPLE.SAMPLE_ID JOIN MSI ON MSI.ID = MSI_SAMPLE.MSI_ID";
-			jdbcTemplate.query(sql, new CSVCallbackHandler(csvPrinter));
-		}
-
-		try (final CSVPrinter csvPrinter = new CSVPrinter(new FileWriter(new File("sample_membership.csv")),
-				CSVFormat.DEFAULT.withHeader(":START_ID(Sample)", ":END_ID(Group)"))) {
-			log.info("Running sample membership query");
-			String sql = "SELECT BIO_PRODUCT.ACC, BIO_SMP_GRP.ACC FROM BIO_SMP_GRP JOIN BIO_SAMPLE_SAMPLE_GROUP ON BIO_SMP_GRP.ID = BIO_SAMPLE_SAMPLE_GROUP.GROUP_ID JOIN BIO_PRODUCT ON BIO_PRODUCT.ID = BIO_SAMPLE_SAMPLE_GROUP.SAMPLE_ID";
-			jdbcTemplate.query(sql, new CSVCallbackHandler(csvPrinter));
-		}
-
 	}
 }
