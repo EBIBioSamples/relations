9321748353cc31e541cc656baf623ee062d425d0
Adam Faulconbridge <afaulconbridge@googlemail.com>
2016-04-27 14:44:13 +0100

    Remove link from csv to neo4j

--- importrelational/src/main/java/uk/ac/ebi/biosamples/relations/importbiosdmodel/Application.java
+++ importrelational/src/main/java/uk/ac/ebi/biosamples/relations/importbiosdmodel/Application.java
@@ -1,36 +1,13 @@
 package uk.ac.ebi.biosamples.relations.importbiosdmodel;
 
-import org.neo4j.ogm.session.Session;
-import org.neo4j.ogm.session.SessionFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.data.neo4j.config.Neo4jConfiguration;
-import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
-import org.springframework.data.neo4j.server.Neo4jServer;
-import org.springframework.data.neo4j.server.RemoteServer;
-import org.springframework.data.neo4j.template.Neo4jOperations;
-import org.springframework.data.neo4j.template.Neo4jTemplate;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-// name of package containing repositories here
-@EnableNeo4jRepositories("uk.ac.ebi.biosamples.relations.repo")
-@EnableTransactionManagement
-//turn on advanced proxy object creation so the multi-threading and repositories work
-@EnableAspectJAutoProxy(proxyTargetClass = true) 
-@PropertySource("ogm.properties")
-public class Application extends Neo4jConfiguration {
-
-	
-	private Logger log = LoggerFactory.getLogger(this.getClass());
-	
+public class Application {	
 	@Value("${URI}")
 	private String uri;
 	@Value("${username}")
@@ -43,32 +20,6 @@ public class Application extends Neo4jConfiguration {
 	public static PropertySourcesPlaceholderConfigurer propertyConfigIn() {
 		return new PropertySourcesPlaceholderConfigurer();
 	}
-	
-	@Override
-	@Bean
-	public SessionFactory getSessionFactory() {
-		// name of package containing domain objects here
-		return new SessionFactory("uk.ac.ebi.biosamples.relations.model");
-	}
-	
-	@Override
-	@Bean
-	public Session getSession() throws Exception {
-		return super.getSession();
-	}
-
-	@Bean
-	public Neo4jOperations getNeo4jTemplate() throws Exception {
-		return new Neo4jTemplate(getSession());
-	}
-	
-	//This will not be needed after spring-data-neo4j v4.0.0
-	@Bean
-    public Neo4jServer neo4jServer() {
-		log.debug("uri "+uri);
-		log.debug("username "+username);
-        return new RemoteServer(uri, username, password);
-    }
 	
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
