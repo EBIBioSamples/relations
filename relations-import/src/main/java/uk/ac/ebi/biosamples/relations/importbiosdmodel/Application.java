71aa7809b85ea82ed8359687acf5ca3a93969bd3
Adam Faulconbridge <afaulconbridge@googlemail.com>
2016-04-27 16:58:15 +0100

    Remove lingering neo4j config

--- relations-import/src/main/java/uk/ac/ebi/biosamples/relations/importbiosdmodel/Application.java
+++ relations-import/src/main/java/uk/ac/ebi/biosamples/relations/importbiosdmodel/Application.java
@@ -1,6 +1,5 @@
 package uk.ac.ebi.biosamples.relations.importbiosdmodel;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,12 +7,6 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @SpringBootApplication
 public class Application {	
-	@Value("${URI}")
-	private String uri;
-	@Value("${username}")
-	private String username;
-	@Value("${password}")
-	private String password;
 	
 	//this is needed to read nonstrings from properties files
 	@Bean
