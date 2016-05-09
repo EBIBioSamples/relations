4b453bb9bf02c3b2d0e8b8b426a7c3babdb25af0
LLTommy <Thomas.Liener@felix-solution.com>
2016-05-09 13:53:11 +0100

    Preparation of adding hardcoded urls to _links

--- relations-webapp/src/main/java/uk/ac/ebi/biosamples/relations/webapp/WebApp.java
+++ relations-webapp/src/main/java/uk/ac/ebi/biosamples/relations/webapp/WebApp.java
@@ -9,6 +9,15 @@ import org.springframework.data.neo4j.config.Neo4jConfiguration;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+
+/*  Imports needed for the ResourceProcessor  */
+import org.springframework.hateoas.ResourceProcessor;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resource;
+import uk.ac.ebi.biosamples.relations.model.Sample;
+import uk.ac.ebi.biosamples.relations.model.Group;
+
+
 /**
  * Created by tliener on 20/04/2016.
  */
@@ -40,6 +49,45 @@ public class WebApp  extends Neo4jConfiguration {
         SpringApplication.run(WebApp.class, args);
 
     }
+
+
+
+
+    /*
+    * This function adds a Link to the Sample resource
+    * */
+    @Bean
+    public ResourceProcessor<Resource<Sample>> sampleProcessor(){
+        return new ResourceProcessor<Resource<Sample>>(){
+            @Override
+            public Resource<Sample> process(Resource <Sample> resource)
+            {
+                //get the Sample accession through the resource object, add this to the new Link in order to produce valid link
+                String accession=resource.getContent().getAccession();
+                resource.add(new Link("http://whatever - for sample "+accession, "Additional Information"));
+                return resource;
+            }
+
+        };
+    }
+
+    /*
+    * This function adds a Link to the Group resource
+    * */
+    @Bean
+    public ResourceProcessor<Resource<Group>> groupProcessor(){
+        return new ResourceProcessor<Resource<Group>> () {
+            @Override
+            public  Resource<Group> process(Resource <Group> resource)
+            {
+                //get the GROUPs accession through the resource object, add this to the new Link in order to produce valid link
+                String accession=resource.getContent().getAccession();
+                resource.add(new Link("http://whatever for group "+accession, "Additional Information"));
+                return resource;
+            }
+        };
+    }
+
 
 
 }
