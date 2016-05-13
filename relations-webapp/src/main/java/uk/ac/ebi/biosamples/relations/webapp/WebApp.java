df55bad5f5cce5d4e144f33e13fa80e9eadc8a4f
LLTommy <Thomas.Liener@felix-solution.com>
2016-05-13 15:24:19 +0100

    Include embedded driver via application properties

--- relations-webapp/src/main/java/uk/ac/ebi/biosamples/relations/webapp/WebApp.java
+++ relations-webapp/src/main/java/uk/ac/ebi/biosamples/relations/webapp/WebApp.java
@@ -5,10 +5,13 @@ import org.neo4j.ogm.session.SessionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.data.neo4j.config.Neo4jConfiguration;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+
+import org.neo4j.ogm.config.Configuration;
 
 /*  Imports needed for the ResourceProcessor  */
 import org.springframework.hateoas.ResourceProcessor;
@@ -16,6 +19,9 @@ import org.springframework.hateoas.Link;
 import org.springframework.hateoas.Resource;
 import uk.ac.ebi.biosamples.relations.model.Sample;
 import uk.ac.ebi.biosamples.relations.model.Group;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.*;
 
 
 /**
@@ -29,18 +35,37 @@ import uk.ac.ebi.biosamples.relations.model.Group;
 public class WebApp  extends Neo4jConfiguration {
 
 
+    @Value("${ogm.uri}")
+    private String uri;
+
+    @Value("${ogm.driver:error}")
+    private String driver;
+
+
+    //****** Try to get rid of ogm.properties by defining neo4 driver within java
+    @Bean
+    public Configuration getConfiguration() {
+        Configuration config = new Configuration();
+        config
+                .driverConfiguration()
+                .setDriverClassName(driver)
+                .setURI(uri);
+        return config;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyConfigIn() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    //****** End experimental part
+
+
     @Override
     @Bean
     public SessionFactory getSessionFactory() {
         // name of package containing domain objects here
-        return new SessionFactory("uk.ac.ebi.biosamples.relations.model");
-    }
-
-
-    @Override
-    @Bean
-    public Session getSession() throws Exception {
-        return super.getSession();
+        return new SessionFactory(getConfiguration(), "uk.ac.ebi.biosamples.relations.model");
     }
 
 
@@ -65,6 +90,19 @@ public class WebApp  extends Neo4jConfiguration {
                 //get the Sample accession through the resource object, add this to the new Link in order to produce valid link
                 String accession=resource.getContent().getAccession();
                 resource.add(new Link("http://whatever - for sample "+accession, "Additional Information"));
+
+
+
+                /* Not working because links are populated after this, :(((
+                System.out.println("In ResourceProcessor");
+                System.out.println(resource.getContent().getParent());
+                System.out.println(resource.getLinks());
+                List <Link> tmpLinkList;
+                tmpLinkList=resource.getLinks();
+                System.out.println(tmpLinkList);
+                System.out.println();*/
+
+
                 return resource;
             }
 
