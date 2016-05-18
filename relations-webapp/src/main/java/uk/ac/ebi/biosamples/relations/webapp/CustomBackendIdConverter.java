3d9223a88be42bd205bbe0898a084af6aea27462
Adam Faulconbridge <afaulconbridge@googlemail.com>
2016-05-18 08:37:38 +0100

    Cleanup and use else/if

--- relations-webapp/src/main/java/uk/ac/ebi/biosamples/relations/webapp/CustomBackendIdConverter.java
+++ relations-webapp/src/main/java/uk/ac/ebi/biosamples/relations/webapp/CustomBackendIdConverter.java
@@ -11,77 +11,63 @@ import uk.ac.ebi.biosamples.relations.repo.SampleRepository;
 import uk.ac.ebi.biosamples.relations.repo.GroupRepository;
 import uk.ac.ebi.biosamples.relations.repo.SubmissionRepository;
 
-
 import java.io.Serializable;
 
 /**
  * Created by tliener on 22/04/2016.
  */
 
-
 @Component
 public class CustomBackendIdConverter implements BackendIdConverter {
 
-    @Autowired
-    SampleRepository sampleRepository;
+	@Autowired
+	SampleRepository sampleRepository;
 
-    @Autowired
-    GroupRepository groupRepository;
+	@Autowired
+	GroupRepository groupRepository;
 
-    @Autowired
-    SubmissionRepository submissionRepository;
+	@Autowired
+	SubmissionRepository submissionRepository;
 
+	@Override // This function has to return the graphid, given a certain
+				// accession
+	public Serializable fromRequestId(String id, Class<?> entityType) {
+		if (entityType.equals(Sample.class)) {
+			return sampleRepository.findOneByAccession(id).getId();
+		} else if (entityType.equals(Group.class)) {
+			return groupRepository.findOneByAccession(id).getId();
+		} else if (entityType.equals(Submission.class)) {
+			return submissionRepository.findOneBySubmissionId(id).getId();
+		} else {
+			throw new IllegalArgumentException("Unrecognized class " + entityType);
+		}
+	}
 
-    @Override // This function has to return the graphid, given a certain accession
-    public Serializable fromRequestId(String id, Class<?> entityType){
-        if (entityType.equals(Sample.class)){
-            return sampleRepository.findOneByAccession(id).getId();
-        }
+	@Override // This function has to return the accession to a given graphid
+	public String toRequestId(Serializable id, Class<?> entityType) {
+		if (entityType.equals(Sample.class)) {
+			return sampleRepository.findOne((Long) id).getAccession();
+		} else if (entityType.equals(Group.class)) {
+			return groupRepository.findOne((Long) id).getAccession();
+		} else if (entityType.equals(Submission.class)) {
+			return submissionRepository.findOne((Long) id).getSubmissionId();
+		} else {
+			throw new IllegalArgumentException("Unrecognized class " + entityType);
+		}
 
-        if (entityType.equals(Group.class)){
-            return groupRepository.findOneByAccession(id).getId();
-        }
+	}
 
-        if (entityType.equals(Submission.class))
-        {
-            return submissionRepository.findOneBySubmissionId(id).getId();
-        }
-
-        //This should NEVER be reached
-        else{
-            System.out.println("Error in BackendidConverter");
-            return "Error in BackendidConverter";
-        }
-    }
-
-
-    @Override //This function has to return the accession to a given graphid
-    public String toRequestId(Serializable id, Class<?> entityType){
-    if (entityType.equals(Sample.class)){
-        return sampleRepository.findOne((Long)id).getAccession();
-    }
-    if (entityType.equals(Group.class)){
-        return groupRepository.findOne((Long) id).getAccession();
-    }
-
-    if (entityType.equals(Submission.class))
-    {
-        return submissionRepository.findOne((Long) id).getSubmissionId();
-    }
-
-    //This should NEVER be reached!
-    else {
-            System.out.println("Error in BackendidConverter");
-            return "Error in BackendidConverter";
-    }
-
-    }
-
-
-
-    @Override
-    public boolean supports(Class<?> delimiter) {
-        return true;
-    }
+	@Override
+	public boolean supports(Class<?> delimiter) {
+		if (delimiter.equals(Sample.class)) {
+			return true;
+		} else if (delimiter.equals(Group.class)) {
+			return true;
+		} else if (delimiter.equals(Submission.class)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 }
