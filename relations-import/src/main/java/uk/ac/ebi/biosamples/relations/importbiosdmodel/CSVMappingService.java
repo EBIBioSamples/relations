2e2bb0215d514f550b646e001a2a7a26200f7047
LLTommy <Thomas.Liener@felix-solution.com>
2016-06-09 10:55:26 +0100

    Added some documentation, NO functionallity changes with this commit

--- relations-import/src/main/java/uk/ac/ebi/biosamples/relations/importbiosdmodel/CSVMappingService.java
+++ relations-import/src/main/java/uk/ac/ebi/biosamples/relations/importbiosdmodel/CSVMappingService.java
@@ -26,6 +26,12 @@ import uk.ac.ebi.fg.myequivalents.model.Entity;
 
 import java.util.*;
 
+/*
+* The class is responsible for actually creating the csv files that are used in the neo importer. For every node as well as relationship
+* an own csv file is created. To create each csv files, the database is queried via Hibernate, the sample and group objects are run through
+* as well as their relationships and the data stored in csv files accordingly. As additional property, Database references for sample/groups are
+* extracted from the object or the myEquivalent database
+* */
 @Service
 public class CSVMappingService implements Closeable {
 
@@ -49,6 +55,9 @@ public class CSVMappingService implements Closeable {
 	//private CSVPrinter nieceOrNephewPrinter;
 
 
+	/*
+	* Sets up csv printers for every sample/relationship by using FileWriter and BufferedWriter
+	* */
 	@PostConstruct
 	public void doSetup() throws IOException {
 		//TODO create in temp dirs, then atomically swap into file location
@@ -66,6 +75,9 @@ public class CSVMappingService implements Closeable {
 		//nieceOrNephewPrinter = new CSVPrinter(new BufferedWriter(new FileWriter(new File(outpath, "nieceOrNephew.csv"))), CSVFormat.DEFAULT);
 	}
 
+	/*
+	* Close every printer when finished
+	* */
 	@PreDestroy
 	public void close() throws IOException {
 		samplePrinter.close();
@@ -124,57 +136,86 @@ public class CSVMappingService implements Closeable {
 
 	}
 
-	/*Print Sample without Db reference*/
+	/*Print Sample Node without Db reference
+	* @param acc accession for a Sample node
+	* */
 	private synchronized void printSample(String acc) throws IOException{
 		samplePrinter.print(acc);
 		samplePrinter.println();
 	}
 
-	/* Print group without Database reference*/
+	/* Print group Node without Database reference
+	*  @param acc accession for a Group node
+	* */
 	private synchronized void printGroup(String acc) throws IOException{
 		groupPrinter.print(acc);
 		groupPrinter.println();
 	}
 
-
+	/* Print Sample Ownership relationship (sample - submission)
+	* @param accession accession of a node
+	* @param subId submission ID
+	* */
 	private synchronized void printSampleOwnership(String acc, String subId) throws IOException {
 		ownershipSamplePrinter.print(acc);
 		ownershipSamplePrinter.print(subId);
 		ownershipSamplePrinter.println();
 	}
 
-
+	/*Print Group Ownership relationship (group - submission)
+	* @param accession accession of a group
+	* @param subId submission ID
+	* */
 	private synchronized void printGroupOwnership(String acc, String subId) throws IOException {
 		ownershipGroupPrinter.print(acc);
 		ownershipGroupPrinter.print(subId);
 		ownershipGroupPrinter.println();
 	}
-	
+
+	/*Print sample to group Membership
+	* @param sampleAcc accession of the sample
+	* @param groupAcc accession of the group
+	* */
 	private synchronized void printMembership(String sampleAcc, String groupAcc) throws IOException {
 		membershipPrinter.print(sampleAcc);
 		membershipPrinter.print(groupAcc);
 		membershipPrinter.println();
 	}
-	
+
+	/*Prints derivation relationship
+	* @param productAcc sample accession
+	* @param sourceAcc sample accession
+	* */
 	private synchronized void printDerivation(String productAcc, String sourceAcc) throws IOException {
 		derivationPrinter.print(productAcc);
 		derivationPrinter.print(sourceAcc);
 		derivationPrinter.println();
 	}
 
+	/*Prints as as relationship
+	* @param sample accession
+	* @param sample accession
+	* */
 	private synchronized void printSameAs(String acc, String otherAcc) throws IOException {
 		sameAsPrinter.print(acc);
 		sameAsPrinter.print(otherAcc);
 		sameAsPrinter.println();
 	}
 
-
+	/*Prints childOf relationship
+	* @param sample accession
+	* @param sample accession
+	* */
 	private synchronized void printChildOf(String acc, String otherAcc) throws IOException {
 		childOfPrinter.print(acc);
 		childOfPrinter.print(otherAcc);
 		childOfPrinter.println();
 	}
 
+	/*Prints Recurated From relationship
+	* @param sample accession
+	* @param sample accession
+	* */
 	private synchronized void printRecuratedFrom(String acc, String otherAcc) throws IOException{
 		ReCuratedFromPrinter.print(acc);
 		ReCuratedFromPrinter.print(otherAcc);
@@ -189,6 +230,10 @@ public class CSVMappingService implements Closeable {
 	}*/
 
 
+	/*
+	* Main function that actually runs through the samples and groups, gets the relevant data and calles the printer functions
+	* @param msi Submission msi
+	* */
 	public void handle(MSI msi) throws IOException {
 		
 		if (valid(msi)) {
